01f6a55f3f9b055ee4940f37ec9c80b9790a388c
Alex Deddo <savoca@codefi.re>
2016-07-16 13:29:32 -0400

    lge_qos: Fix wrong type for dt2w
    Change-Id: I3a4da0cd17635d44edfa22d70e4846da9f105141

--- drivers/input/touchscreen/lge_qos/touch_core.c
+++ drivers/input/touchscreen/lge_qos/touch_core.c
@@ -839,7 +839,7 @@ void touch_send_uevent(struct touch_core_data *ts, int type)
 				KOBJ_CHANGE, uevent_str[type]);
 		TOUCH_I("%s\n",  uevent_str[type][0]);
 		touch_report_all_event(ts);
-	} else if (type == LPWG_DOUBLE_TAP) {
+	} else if (type == TOUCH_UEVENT_KNOCK) {
 		input_report_key(ts->input, KEY_WAKEUP, 1);
 		TOUCH_I("Simulate power button depress");
 		input_sync(ts->input);
