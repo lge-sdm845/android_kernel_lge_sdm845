90f43dca603c6ed213b06a4e6f4e82a3c2fcbaad
Rashed Abdel-Tawab <rashed@linux.com>
2016-07-30 01:53:28 -0400

    lge_touch: Add newline chars to power button simmulation messages

--- drivers/input/touchscreen/lge_qos/touch_core.c
+++ drivers/input/touchscreen/lge_qos/touch_core.c
@@ -841,10 +841,10 @@ void touch_send_uevent(struct touch_core_data *ts, int type)
 		touch_report_all_event(ts);
 	} else if (type == TOUCH_UEVENT_KNOCK) {
 		input_report_key(ts->input, KEY_WAKEUP, 1);
-		TOUCH_I("Simulate power button depress");
+		TOUCH_I("Simulate power button depress\n");
 		input_sync(ts->input);
 		input_report_key(ts->input, KEY_WAKEUP, 0);
-		TOUCH_I("Simulate power button release");
+		TOUCH_I("Simulate power button release\n");
 		input_sync(ts->input);
 	} else {
 		reinit_completion(&ts->uevent_complete);
