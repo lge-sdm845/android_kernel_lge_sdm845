83dcea61acb816553ff29576d578b2df047b388c
Han Sol Jin <hansol@hansol.ca>
2023-12-16 07:27:46 -0800

    touchscreen: lge_qos: sw42902: add udfps_pressed sysfs
    To report whether fingerprint sensor is being held.
    
    Change-Id: I6e15d628ff4c50874dc727d520ef23e06e3e8bbf
    Signed-off-by: Han Sol Jin <hansol@hansol.ca>

--- drivers/input/touchscreen/lge_qos/lgsic/sw42902/touch_sw42902.c
+++ drivers/input/touchscreen/lge_qos/lgsic/sw42902/touch_sw42902.c
@@ -4809,6 +4809,7 @@ int sw42902_irq_lpwg(struct device *dev)
 		sw42902_get_tci_data(dev, 1);
 		ts->intr_status = TOUCH_IRQ_LPWG_LONGPRESS_DOWN;
 		d->longpress_uevent_status = ts->intr_status;
+		sysfs_notify(&ts->kobj, NULL, "udfps_pressed");
 		break;
 
 	case LONG_PRESS_UP:
@@ -5751,6 +5752,14 @@ static ssize_t show_gpio_pin(struct device *dev, char *buf)
 	return ret;
 }
 
+int udfps_pressed_status = 0;
+
+static ssize_t show_udfps_pressed(struct device *dev, char *buf)
+{
+	return scnprintf(buf, PAGE_SIZE, "%d\n", udfps_pressed_status);
+}
+
+static TOUCH_ATTR(udfps_pressed, show_udfps_pressed, NULL);
 static TOUCH_ATTR(lpwg_abs, show_lpwg_abs, store_lpwg_abs);
 static TOUCH_ATTR(reg_ctrl, NULL, store_reg_ctrl);
 static TOUCH_ATTR(lpwg_failreason, show_lpwg_failreason, store_lpwg_failreason);
@@ -5769,6 +5778,7 @@ static TOUCH_ATTR(gpio_pin, show_gpio_pin, NULL);
 //static TOUCH_ATTR(burst_test, NULL, store_burst_test);
 
 static struct attribute *sw42902_attribute_list[] = {
+	&touch_attr_udfps_pressed.attr,
 	&touch_attr_reg_ctrl.attr,
 	&touch_attr_lpwg_failreason.attr,
 	&touch_attr_lpwg_abs.attr,
