33d34a966e6ddae229968c726d2d4a7f22d7e69e
Han Sol Jin <hansol@hansol.ca>
2024-07-26 12:06:05 -0700

    treewide: eradicate USB_HOST_NOTIFY
    Change-Id: Ief93afc04fb4a848e421fff24bc84e06803e2764
    Signed-off-by: Han Sol Jin <hansol@hansol.ca>

--- include/scsi/scsi_host.h
+++ include/scsi/scsi_host.h
@@ -739,9 +739,6 @@ struct Scsi_Host {
 	 * Needed just in case we have virtual hosts.
 	 */
 	struct device *dma_dev;
-#ifdef CONFIG_USB_HOST_NOTIFY
-	unsigned int  by_usb;
-#endif
 #ifdef CONFIG_SCSI_DEVICE_IDENTIFIER
 	unsigned int  by_ufs;
 #endif
